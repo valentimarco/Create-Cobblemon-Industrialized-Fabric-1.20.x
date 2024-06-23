42dfa35a8821d5b6233b9438542dedd92bf979f5
Lord_Of_Crumbs <zaarons08@gmail.com>
2024-06-23 13:36:01 -0700

    Innitial commit

---
+++ src/main/java/net/crumbs/createcobblemon/CreateCobblemonIndustrialized.java
@@ -0,0 +1,17 @@
+package net.crumbs.createcobblemon;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreateCobblemonIndustrialized implements ModInitializer {
+	public static final String MOD_ID = "create_cobblemon";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/crumbs/createcobblemon/CreateCobblemonIndustrializedClient.java
@@ -0,0 +1,10 @@
+package net.crumbs.createcobblemon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateCobblemonIndustrializedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
