7fa936a32a10fac21bf14a3e51684be6627d4394
LucasTheDrgn <kirbyfan.95@gmail.com>
2024-11-22 19:03:15 -0500

    Adding potions and reorganizing recipes

--- src/main/java/net/crumbs/createcobblemon/CreateCobblemonIndustrializedClient.java
+++ src/main/java/net/crumbs/createcobblemon/CreateCobblemonIndustrializedClient.java
@@ -1,10 +1,11 @@
 package net.crumbs.createcobblemon;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.crumbs.createcobblemon.fluids.ModFluids;
 
 public class CreateCobblemonIndustrializedClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ModFluids.onInitializeClient();
     }
 }
